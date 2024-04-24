01aa6f30b5792f9157093432748692b293f7de5d
Vijay Krishna Chauhan <89722230+vijay-krishna-chauhan@users.noreply.github.com>
2024-04-24 12:02:43 +0530

    openai api key error corrected

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,20 +1,21 @@
-export const LOGO="https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png";
+export const LOGO =
+  "https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png";
 
-export const USER_AVATAR="https://occ-0-6247-2164.1.nflxso.net/dnm/api/v6/K6hjPJd6cR6FpVELC5Pd6ovHRSk/AAAABdpkabKqQAxyWzo6QW_ZnPz1IZLqlmNfK-t4L1VIeV1DY00JhLo_LMVFp936keDxj-V5UELAVJrU--iUUY2MaDxQSSO-0qw.png?r=e6e";
+export const USER_AVATAR =
+  "https://occ-0-6247-2164.1.nflxso.net/dnm/api/v6/K6hjPJd6cR6FpVELC5Pd6ovHRSk/AAAABdpkabKqQAxyWzo6QW_ZnPz1IZLqlmNfK-t4L1VIeV1DY00JhLo_LMVFp936keDxj-V5UELAVJrU--iUUY2MaDxQSSO-0qw.png?r=e6e";
 
-export const IMG_CDN_URL="https://image.tmdb.org/t/p/w500";
+export const IMG_CDN_URL = "https://image.tmdb.org/t/p/w500";
 
-export const BG_URL="https://assets.nflxext.com/ffe/siteui/vlv3/93da5c27-be66-427c-8b72-5cb39d275279/94eb5ad7-10d8-4cca-bf45-ac52e0a052c0/IN-en-20240226-popsignuptwoweeks-perspective_alpha_website_large.jpg"
+export const BG_URL =
+  "https://assets.nflxext.com/ffe/siteui/vlv3/93da5c27-be66-427c-8b72-5cb39d275279/94eb5ad7-10d8-4cca-bf45-ac52e0a052c0/IN-en-20240226-popsignuptwoweeks-perspective_alpha_website_large.jpg";
 
-
- //export const OPENAI_KEY=process.env.REACT_APP_OPENAI_KEY;
- export const OPENAI_KEY="sk-proj-rNi4iNv6LTHiLXzxKnCkT3BlbkFJJtqqUHtpPYmKwlt3q3Bz";
 
 export const API_OPTIONS = {
-
-  method: 'GET',
+  method: "GET",
   headers: {
-    accept: 'application/json',
+    accept: "application/json",
     Authorization: "Bearer " + process.env.REACT_APP_TMDB_KEY,
-  }
+  },
 };
+
+export const OPENAI_KEY = process.env.REACT_APP_OPENAI_KEY;
